d414217b9e326de2b71733b6d403d2f0805f03d4
DeekshithSH <shdeekshith@gmail.com>
2024-08-31 20:56:53 +0530

    updated vars.py

--- project/utils/vars.py
+++ project/utils/vars.py
@@ -10,7 +10,10 @@ class Var:
     DB_PORT:int = int(environ.get("DB_PORT", 3306))
     PORT:int = int(environ.get("PORT", 8080))
     HOST:str = environ.get("HOST", "0.0.0.0")
-    URL:str = f"http://{HOST}:{PORT}"
+    FQDN:str = environ.get("FQDN", HOST)
+    HAS_SSL=environ.get("HAS_SSL", False)
+    NO_PORT=environ.get("NO_PORT", False)
+    URL:str = "http{}://{}{}".format("s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":"+str(PORT))
     CF_CLIENTID: str = environ.get("CF_CLIENTID")
     CF_CLIENTSECRET: str = environ.get("CF_CLIENTSECRET")
     CF_VERSION: str = environ.get("CF_VERSION", "2023-08-01")
